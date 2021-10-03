4caa3b141e41dcfe0ab802f29ad22e9c175b0e39
hitoriudon <tjrcjf9@hanyang.ac.kr>
2021-10-03 16:49:00 +0900

    selenium example

---
+++ example1.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Options는 생략 가능하다
+options = webdriver.ChromeOptions()
+
+# 창 크기 설정
+options.add_argument('window-size=1280,720')
+
+# ...또는 창 최대화
+#options.add_argument('start-maximized')
+
+# ...또는 헤드리스(창이 보이지 않는) 모드
+options.add_argument('--headless')
+options.add_argument('--disable-gpu') 
+'''윈도우인 사람은 on, 아니면 off'''
+ 
+# 드라이버 생성
+driver = webdriver.Chrome()
+#('chromedriver', options = options) '''이건 아무래도 윈도우 전용인 것 같다..'''
+
+# 로딩 기다리기
+driver.implicitly_wait(5)
+
+# 구글 접속
+driver.get('https://www.google.com')
+
+# 검색창 찾기
+#search = driver.find_element_by_name('q')
+#search = driver.find_element_by_class_name('gLFyf') 
+'''html class의 이름으로 찾을건데, 그 클래스의 이름은 gLFyf입니다. -> 근데 만약, 클래스 이름이 겹치면????'''
+'''그래서 ! 셀렉터로 해결~~! '''
+'''원본->''' #search = driver.find_element_by_css_selector('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input')
+search = driver.find_element_by_css_selector('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input')
+
+# 검색어 입력
+search.send_keys('한양대학교 포리프')
+
+# 검색어 삭제
+search.clear()
+
+# 검색어 입력
+search.send_keys('한양대학교 FORIF')
+
+# 검색어 제출
+#search.submit()
+
+# 또는 엔터키 입력
+search.send_keys(Keys.RETURN)
+
+# 페이스북 링크 선택
+facebook_link = driver.find_element_by_css_selector('#rso > div:nth-child(1) > div > div:nth-child(1) > div > div > div > div.yuRUbf > a > h3')
+
+# 링크 클릭
+facebook_link.click()
+
+# 뒤로 가기
+driver.back() 
+
+# 앞으로 가기
+driver.forward() 
+
+# 현재 주소 출력
+print(driver.current_url)
+
+# 드라이버 닫기
+driver.quit()
---
+++ example2.py
@@ -0,0 +1,63 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+
+# 파일에서 ID, 비밀번호 읽기
+id_file = open('id', 'r')
+pw_file = open('pw', 'r')
+
+id_str = id_file.read()
+pw_str = pw_file.read()
+
+id_file.close()
+pw_file.close()
+
+# 웹 드라이버 옵션 만들기
+options = webdriver.ChromeOptions()
+
+#options.add_argument('start-maximized')
+options.add_argument('--headless')
+#options.add_argument('--disable-gpu') 
+
+# 웹 드라이버 만들기
+driver = webdriver.Chrome() #수정햇어여
+driver.implicitly_wait(10)
+
+# LMS 사이트 접속하기
+driver.get('http://learning.hanyang.ac.kr')
+
+# 로그인 하기 ''' 크롬 검사 두 번 눌러서.. 아이디 값을 복사해서.. 그런 느낌이예요 확실히 검사를 진짜 많이 쓰긴 하겠당'''
+id_box = driver.find_element_by_id('uid')
+pw_box = driver.find_element_by_id('upw')
+login  = driver.find_element_by_id('login_btn')
+
+id_box.send_keys(id_str)
+pw_box.send_keys(pw_str)
+login.click()
+
+try:
+    alert = Alert(driver)
+
+    # 비밀번호를 바꾸라는 경고
+    if alert.text.startswith('한양대학교 포털 한양인에서는 회원님의 개인정보를 안전하게 보호하고'):
+        alert.accept()
+    # 아이디 혹은 비밀번호가 틀렸을 때
+    else:
+        alert.accept()
+        print('아이디 혹은 비밀번호가 틀렸습니다.')
+        sys.exit()
+except:
+    pass
+
+# 과목들 가져오기 
+''' 과목들을 가져오고 싶은데, 과목이 한 개가 아니니까 과목 html 검사해서 공통적인 class든 id든 찾아야함. 그게 바로 밑에 줄 코드!'''
+classes = driver.find_elements_by_class_name('ic-DashboardCard__header-subtitle')
+
+# 과목들 출력
+for cs in classes:
+    print(cs.text)
+ 
+# 웹 드라이버 종료
+#driver.quit()
+
+'''problem: 터미널에 결과값이 안 떠... 왜 내 과목들 뜨지 않는거야.? ? '''
