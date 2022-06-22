465746fb90292a65bc87b4f230bc3958b829b8cc
hitoriudon <tjrcjf9@hanyang.ac.kr>
2022-06-22 09:01:50 +0900

    Q1 deque

--- PAI/Valid Palindrome.py
+++ PAI/Valid Palindrome.py
@@ -1,10 +1,27 @@
 '''
 palindrome = 회문. 앞 뒤가 똑같은 단어나 문장.
 '''
+import collections
+
+
 text = "A man, a plan, a canal: Panama"
-words = ''.join(c for c in text if c.isalnum()).lower()
-for i in range (len(words)//2):
-    if words[i] != words[-(i+1)]: 
-        print(False)
-        break
-else: print(True)
+
+def my_sol(): #304ms for executing
+    words = ''.join(c for c in text if c.isalnum()).lower()
+    for i in range (len(words)//2):
+        if words[i] != words[-(i+1)]: 
+            return False
+    return True
+
+def deque_sol(): #64ms for executing
+    strs = collections.deque()
+    for char in text:
+        if char.isalnum():
+            strs.append(char.lower())        
+    while len(strs)>1:
+        if strs.popleft() != strs.pop(): #popleft() time complexity is O(1)
+            return False
+    return True
+
+print(deque_sol())
+print(my_sol())
