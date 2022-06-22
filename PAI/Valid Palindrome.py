359f804a8ad7d40281538e68cd19c8fc43749ac0
hitoriudon <tjrcjf9@hanyang.ac.kr>
2022-06-22 11:12:54 +0900

    Q1 slicing

--- PAI/Valid Palindrome.py
+++ PAI/Valid Palindrome.py
@@ -2,18 +2,19 @@
 palindrome = 회문. 앞 뒤가 똑같은 단어나 문장.
 '''
 import collections
-
+import re
 
 text = "A man, a plan, a canal: Panama"
 
-def my_sol(): #304ms for executing
+def my_sol(): 
     words = ''.join(c for c in text if c.isalnum()).lower()
+    #return words == words[::-1]
     for i in range (len(words)//2):
         if words[i] != words[-(i+1)]: 
             return False
     return True
 
-def deque_sol(): #64ms for executing
+def deque_sol():
     strs = collections.deque()
     for char in text:
         if char.isalnum():
@@ -22,6 +23,11 @@ def deque_sol(): #64ms for executing
         if strs.popleft() != strs.pop(): #popleft() time complexity is O(1)
             return False
     return True
+
+def best_sol():
+    s = text.lower()
+    s = re.sub('[^a-z0-9]', '', s)
+    return s == s[::-1]
 
 print(deque_sol())
 print(my_sol())
